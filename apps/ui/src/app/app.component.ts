79e08a19e43e5eaee7b18b60f2ab8105916e2fff
Patrick Sweeney <psweeney101@gmail.com>
2022-08-31 20:13:25 -0400

    Standardize font sizes

--- apps/ui/src/app/app.component.ts
+++ apps/ui/src/app/app.component.ts
@@ -5,4 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent { }
+export class AppComponent {
+  /** Whether or not the <edwin-time> component should be displayed */
+  get shouldDisplayTime(): boolean {
+    const date = new Date();
+    const hour = date.getHours();
+
+    // Display time while sleeping
+    if (hour < 6) return true;
+    // Display photos while getting ready
+    if (hour < 8) return false;
+    // Display time during the day
+    if (hour < 16) return true;
+    // Display photos during the evening
+    if (hour < 22) return false;
+    // Display time at night
+    return true;
+  }
+}
