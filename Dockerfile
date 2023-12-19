ade2d3957b78b99680c91fa13c9fc7e9d4203bb2
Patrick Sweeney <33041355+psweeney101@users.noreply.github.com>
2023-12-18 22:12:26 -0500

    Dockerize

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM node:alpine
+
+WORKDIR /app
+
+# Install dependencies
+COPY ./dist/apps/api/package.json /app/package.json
+COPY ./package-lock.json /app/package-lock.json
+RUN NODE_ENV=production npm install
+RUN npm install -g pm2
+
+# Copy API
+COPY ./dist/apps/api /app
+
+# Copy UI
+COPY ./dist/apps/ui /app/ui
+ENV NODE_ENV production
+ENV UI_DIST_PATH /app/ui
+
+# Start app
+EXPOSE 3000
+CMD ["pm2-runtime", "main.js"]
--- libs/environments/api/src/index.ts
+++ libs/environments/api/src/index.ts
@@ -1,24 +1,24 @@
 import { config } from 'dotenv';
 
-const { parsed } = config();
-
-if (!parsed) {
-  throw new Error('Failed to load environment variables');
+if (process.env['NODE_ENV'] !== 'production') {
+  if (!config()) {
+    throw new Error('Failed to load environment variables');
+  }
 }
 
 export const environment = {
-  PRODUCTION: parsed['NODE_ENV'] === 'production',
-  API_URL: parsed['API_URL'],
-  UI_URL: parsed['UI_URL'],
-  UI_DIST_PATH: parsed['UI_DIST_PATH'],
+  PRODUCTION: process.env['NODE_ENV'] === 'production',
+  API_URL: process.env['API_URL'] as string,
+  UI_URL: process.env['UI_URL'] as string,
+  UI_DIST_PATH: process.env['UI_DIST_PATH'] as string,
 
-  SPOTIFY_CLIENT_ID: parsed['SPOTIFY_CLIENT_ID'],
-  SPOTIFY_CLIENT_SECRET: parsed['SPOTIFY_CLIENT_SECRET'],
-  SPOTIFY_REFRESH_TOKEN: parsed['SPOTIFY_REFRESH_TOKEN'],
+  SPOTIFY_CLIENT_ID: process.env['SPOTIFY_CLIENT_ID'] as string,
+  SPOTIFY_CLIENT_SECRET: process.env['SPOTIFY_CLIENT_SECRET'] as string,
+  SPOTIFY_REFRESH_TOKEN: process.env['SPOTIFY_REFRESH_TOKEN'] as string,
 
-  PHOTOS_PATH: parsed['PHOTOS_PATH'],
+  PHOTOS_PATH: process.env['PHOTOS_PATH'] as string,
 
-  WIFI_SSID: parsed['WIFI_SSID'],
-  WIFI_PASSWORD: parsed['WIFI_PASSWORD'],
-  WIFI_SECURITY: parsed['WIFI_SECURITY'],
+  WIFI_SSID: process.env['WIFI_SSID'] as string,
+  WIFI_PASSWORD: process.env['WIFI_PASSWORD'] as string,
+  WIFI_SECURITY: process.env['WIFI_SECURITY'] as string,
 };
